0d53d4a457a8cd7b45d8578c041d50dc53ebd287
Safin Haque <safinhq13@gmail.com>
2025-01-26 20:09:38 -0500

    add Student teacher appointment project

--- src/Components/projects.jsx
+++ src/Components/projects.jsx
@@ -29,9 +29,28 @@ export default function Projects(){
               <img id='on-the-record-pic' src='./project-images/on-the-record.png' alt='On the Record UI'/>
           </div>
         </div> 
-        {/* <div className='project left'>
-            <h2>Student/Teacher Appointment Site</h2>
+
+        <div className='project left'>
+        <div id='description'>
+            <h2>Student/Teacher Appointments</h2>
+            <hr></hr>
+            <ul>
+              <li>A fullstack website using Flask/SQL for student/teacher appointment</li>
+              <li>Learned to Implement the MVC design patterns on our project and how to use docker to run it inside of a container</li>
+              <li>Implement basic CRUD operations for middlewear in python for the backend process.</li>
+            </ul>
+            <div id='tools-used-1'>
+                <div>Flask</div>
+                <div>SQL</div>
+                <div>Docker</div>
+                <div>HTML/CSS</div>
+            </div>
+          </div>
+          <div>
+              <img id='on-the-record-pic' src='./project-images/on-the-record.png' alt='On the Record UI'/>
+          </div>
         </div>
+        {/* 
         <div className='project right'>
             <h2>Database Superstore</h2>
         </div>
