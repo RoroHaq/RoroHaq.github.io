c02d7ced43ad769ee458289e0b35f082ff98015b
Safin Haque <safinhq13@gmail.com>
2025-01-26 21:03:07 -0500

    add all projects

--- src/Components/projects.jsx
+++ src/Components/projects.jsx
@@ -26,7 +26,7 @@ export default function Projects(){
             </div>
           </div>
           <div>
-              <img id='on-the-record-pic' src='./project-images/on-the-record.png' alt='On the Record UI'/>
+              <img id='project-image' src='./project-images/on-the-record.png' alt='On the Record UI'/>
           </div>
         </div> 
 
@@ -47,16 +47,42 @@ export default function Projects(){
             </div>
           </div>
           <div>
-              <img id='on-the-record-pic' src='./project-images/on-the-record.png' alt='On the Record UI'/>
+              <img id='project-image' src='./project-images/student-teacher-appointment.png' alt='On the Record UI'/>
           </div>
         </div>
-        {/* 
-        <div className='project right'>
-            <h2>Database Superstore</h2>
+        <div className='project right no-img'>
+        <div id='description'>
+            <h2>Database SuperStore</h2>
+            <hr></hr>
+            <ul>
+              <li>A Java Console Application using JDBC/SQL in which you traverse a superstore as either a client or admin.</li>
+              <li>Contribute on Normalizing our Table, create packages for our procedures using PL/SQL, 
+                as well as making middlewear functions using JDBC.</li>
+              <li>Implement Front-End Console Application for the Client side, connecting the middlewear.</li>
+            </ul>
+            <div id='tools-used-1'>
+                <div>Java</div>
+                <div>SQL & PL/SQL</div>
+                <div>JDBC</div>
+            </div>
+          </div>
         </div>
-        <div className='project left'>
+        <div className='project left no-img'>
+        <div id='description'>
             <h2>Mondial Munch</h2>
-        </div> */}
+            <hr></hr>
+            <ul>
+              <li>A C# Application with Avalonia UI which is a recipe app that uses a globe to track recipes you made from the origin.</li>
+              <li>Collaborate in a team of 3 implementing required classes, Avalonia, DB & Unit Testing by using Moq</li>
+              <li>Contribute making the basic Avalonia UI and functionality to interact with our DB to write reviews, give ratings and create new recipes</li>
+            </ul>
+            <div id='tools-used-1'>
+                <div>C#</div>
+                <div>Avalonia</div>
+                <div>SQLite</div>
+            </div>
+          </div>
+        </div> 
       </div>
     </>
   )
