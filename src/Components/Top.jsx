bc51c3c28a754d281185698bb8b5b09696130464
Safin Haque <safinhq13@gmail.com>
2025-02-05 20:06:01 -0500

    add fonts and add proper bio

--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -8,16 +8,16 @@ export default function Top(){
         <div id="top-text">
           <h1>Safin Haque</h1>
           <h3>Junior FullStack Developer</h3>
-          <p>A Dawson Student with Interest in FullStack web Development and Database Management</p>
+          <p>Current 3rd Year Dawson Computer Science Student.</p>
+          <p>Can speak English, French and Bangla. Have an Interest in FullStack Web Development and Database Management. Passionate in working </p>
+          <a href='https://www.linkedin.com/in/safin-haque-7824792aa/' target="_blank">
+            <img id='linkedin-image' src='./icons/icons8-linkedin-50.png'></img>
+          </a>
+          <a href='https://gitlab.com/SafinHQ13' target='_blank'>
+            <img id="gitlab-logo"src='./icons/gitlab-logo-700.png'/>
+          </a>    
         </div>
-        <figure>
-          <img id='profile_image' src='/profile.png'></img>
-          <figcaption>
-            <a href='https://www.linkedin.com/in/safin-haque-7824792aa/' target="_blank">
-              <img id='linkedin-image' src='./icons/icons8-linkedin-50.png'></img>
-            </a>
-          </figcaption>
-        </figure>
+        <img id='profile_image' src='/profile.png'></img>
       </div>
     </>
   )
